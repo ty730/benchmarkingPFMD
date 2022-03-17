03f2a8bcf5daca6839b38a7f82b495a17623aa68
Dylan Stockard <dylstockard@live.com>
2022-03-17 12:09:22 -0700

    Graphs all on one plot for direct comparison
    More useful than separate plots
    Make sure to change the directory

---
+++ python/graph_z_together.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+GROUND_TRUTH_Z = 217.5
+
+
+def main():
+  directory = "./data/" // Directory to your folder of data
+  stats_dict = {"file name": [], "z data": [], "z rmse": [], "n": []}
+  for path in os.listdir(directory):
+    curr = pd.read_csv(directory + path)
+    rmse = (((curr["z_cm"] - GROUND_TRUTH_Z)**2).mean())**(1/2)
+    print("RMSE (cm): " + str(rmse))
+    stats_dict["file name"].append(path)
+    stats_dict["z data"].append(curr['z_cm'])
+    stats_dict["z rmse"].append(rmse)
+    stats_dict["n"].append(len(curr))
+
+  stats_df = pd.DataFrame(stats_dict)
+  count = len(stats_df)
+  
+  for i in range(count):
+    curr_data = stats_df.iloc[i]
+    y_actual = curr_data["z data"]
+    y_pred = [GROUND_TRUTH_Z] * len(y_actual)
+    y = y_actual - y_pred
+    x = [n for n in range(len(y_actual))]
+	
+    plt.plot(x, y, label=curr_data["file name"])
+    plt.title("Z differences")
+    plt.xlabel("sample")
+    plt.ylabel("z difference (cm)")
+
+  plt.legend()
+  plt.show()
+
+
+if __name__ == "__main__":
+    main()
